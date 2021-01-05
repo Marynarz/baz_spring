f5ee2e7a1d756d2a32085e799d41cec5dbfb65f6
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-05 23:47:34 +0100

    odpowiedzi, wsyztskie mogele gotowe

---
+++ ankiety/src/main/java/com/niedz/ankiety/model/Odpowiedz.java
@@ -0,0 +1,45 @@
+package com.niedz.ankiety.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Odpowiedz {
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    private Integer id_pytania;
+
+    private String odpowiedz;
+
+    private Integer ilosc_odpowiedzi = 0;
+
+    // gettery
+    public Integer get_id() { return Id; }
+
+    public Integer get_id_pytania() { return id_pytania; }
+
+    public String get_odpowiedz() { return odpowiedz; }
+
+    public Integer get_ilosc_odpowiedzi() { return ilosc_odpowiedzi; }
+
+    // settery
+    public void set_odpowiedz(String odpowiedz) { this.odpowiedz = odpowiedz; }
+
+    public void add_odpowiedz() { this.ilosc_odpowiedzi++; }
+
+    public void zeruj_ilosc_odpowiedzi() { this.ilosc_odpowiedzi = 0; }
+}
