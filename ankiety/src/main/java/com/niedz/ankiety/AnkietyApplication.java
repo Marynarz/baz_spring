06e74d3b2ef99b9d2b16880081f48c364bf1cf9b
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-05 18:04:30 +0100

    drugie podjescie

---
+++ ankiety/src/main/java/com/niedz/ankiety/AnkietyApplication.java
@@ -0,0 +1,13 @@
+package com.niedz.ankiety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkietyApplication.class, args);
+	}
+
+}
