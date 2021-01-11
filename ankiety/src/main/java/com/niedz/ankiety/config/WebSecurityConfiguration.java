3c3bc443adf8add8cac5068d2085424eaafac1b0
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-11 17:41:14 +0100

    websec dalsze

--- ankiety/src/main/java/com/niedz/ankiety/config/WebSecurityConfiguration.java
+++ ankiety/src/main/java/com/niedz/ankiety/config/WebSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.niedz.ankiety.config;
 
 import com.niedz.ankiety.service.SerwisDetaleUzytkownika;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
