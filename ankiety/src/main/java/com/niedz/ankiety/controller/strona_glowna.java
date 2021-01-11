76925e4aba635809dc368fa16f80253e772b4f0e
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-11 18:50:49 +0100

    dodanie wyswietlania szczegolow uzytkownika

--- ankiety/src/main/java/com/niedz/ankiety/controller/strona_glowna.java
+++ ankiety/src/main/java/com/niedz/ankiety/controller/strona_glowna.java
@@ -8,7 +8,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class strona_glowna {
@@ -28,4 +32,22 @@ public class strona_glowna {
         m_a_v.setViewName("Glowna");
         return m_a_v;
     }
+
+    @GetMapping(value="/uzytkownik/opis")
+    public ModelAndView opis_uzytkownika(){
+        ModelAndView m_a_v = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Uzytkownik uzytkownik = serwis_uzytkownika.znajdzLogin(auth.getName());
+        m_a_v.addObject("uzytkownik",  uzytkownik);
+        m_a_v.addObject("ankiety", serwis_ankiet.pokaz_po_uzytkowniku(uzytkownik.getId()));
+        m_a_v.setViewName("opis_uzytkownika");
+        return m_a_v;
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody
+    Optional<Uzytkownik> pokaz_uzytkownika(@PathVariable("id") int id) {
+        return serwis_uzytkownika.znajdzId(id);
+    }
+
 }
