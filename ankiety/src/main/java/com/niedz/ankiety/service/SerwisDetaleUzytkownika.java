06e6eb27d9e72d81a89b4bdd0a0200b85d917f06
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-11 16:58:38 +0100

    detale usera

--- ankiety/src/main/java/com/niedz/ankiety/service/SerwisDetaleUzytkownika.java
+++ ankiety/src/main/java/com/niedz/ankiety/service/SerwisDetaleUzytkownika.java
@@ -1,12 +1,17 @@
 package com.niedz.ankiety.service;
 
+import com.niedz.ankiety.model.Rola;
 import com.niedz.ankiety.model.Uzytkownik;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SerwisDetaleUzytkownika implements UserDetailsService {
     @Autowired
@@ -16,7 +21,12 @@ public class SerwisDetaleUzytkownika implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         Uzytkownik uzytkownik = serwis_uzytkownika.znajdzLogin(s);
+
+        Set<GrantedAuthority> role = new HashSet<>();
+        for(Rola rola: uzytkownik.get_rola())
+            role.add(new SimpleGrantedAuthority(rola.get_rola()));
+
         return new org.springframework.security.core.userdetails.User(uzytkownik.get_login(),
-                uzytkownik.get_password(), uzytkownik.get_active(), true, true, true,null);
+                uzytkownik.get_password(), uzytkownik.get_active(), true, true, true,role);
     }
 }
