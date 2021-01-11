11cf7f904c53c3f2c5b6f1d566e5b9ee19efbd80
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-11 17:37:18 +0100

    websec

--- ankiety/src/main/java/com/niedz/ankiety/config/WebSecurityConfiguration.java
+++ ankiety/src/main/java/com/niedz/ankiety/config/WebSecurityConfiguration.java
@@ -1,11 +1,27 @@
 package com.niedz.ankiety.config;
 
+import com.niedz.ankiety.service.SerwisDetaleUzytkownika;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private SerwisDetaleUzytkownika detale_uzytkownika;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(detale_uzytkownika).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/static/**", "/css/**", "/images/**", "/img/**");
+    }
 
 }
