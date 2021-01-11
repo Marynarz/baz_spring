3e4e04da0b8717573ffdb5b76999076a6dd83abc
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-11 17:44:48 +0100

    narazei sec wysarczy

--- ankiety/src/main/java/com/niedz/ankiety/config/WebSecurityConfiguration.java
+++ ankiety/src/main/java/com/niedz/ankiety/config/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.niedz.ankiety.service.SerwisDetaleUzytkownika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,8 +25,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/logowanie").permitAll()
+                .antMatchers("/rejestracja").permitAll()
+                .antMatchers("/**").hasAnyAuthority("main", "admin", "user");
+    }
+
+    @Override
     public void configure(WebSecurity web) throws Exception{
         web.ignoring().antMatchers("/static/**", "/css/**", "/images/**", "/img/**");
     }
-
 }
